740c07f22cbb7758781c0f423dbf4a8ea9cd7c93
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2022-02-03 23:19:26 +0530

    Added new practise problem

---
+++ 2. PRACTICE/1. Basic Programs/multi_dimension.py
@@ -0,0 +1,13 @@
+# Multi dimensional dictionaries and lists
+
+lis = [1, 2, 3, 4, 5], ['a', 'b', 'c'], ["hello", "list"]
+print(lis)
+print(lis[0])
+print(lis[0][1])
+print(lis[2][1])
+
+dict1 = {'India': {'Language': 'Hindi', 'population': '100'},
+         'France': {'Language': 'French', 'population': '50'},
+         'China': {'Language': 'Chinese', 'population': '200'}}
+
+print(dict1)
