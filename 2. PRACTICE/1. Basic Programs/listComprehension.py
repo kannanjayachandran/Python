b146ade7a0114e3b8895146bed8294be4973548f
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2022-02-05 23:57:43 +0530

    added new file

--- 2. PRACTICE/1. Basic Programs/listComprehension.py
+++ 2. PRACTICE/1. Basic Programs/listComprehension.py
@@ -1,5 +1,12 @@
 # List comprehension
 
+import numbers
+from re import L
+from xml.dom.minidom import TypeInfo
+from numpy import typecodes
+from pyparsing import Word
+
+
 lis = [x for x in range(10)]
 print(lis)
 
@@ -17,3 +24,17 @@ multi = [x * y for x in [10, 20, 30] for y in [1, 2, 3]]
 print(multi)
 # This is the equivalent of a for loop iterating through [10, 20, 30] and an inside for loop,
 # iterating through [1, 2, 3] for each value of the outside for loop.
+
+# String manipulation using comprehension
+l = 'elephant'
+print([char.upper() for char in l])
+
+friends = ['ron', 'paul', 'zimba']
+print([friend[0].upper() for friend in friends])
+
+# Boolean values with list comprehension
+print([bool(val) for val in [0, '', 1, []]])
+
+# Converting a int list to string
+numbers = [1, 3 , 4, 54, 4, 46]
+number_str = [str(number) for number in numbers]
