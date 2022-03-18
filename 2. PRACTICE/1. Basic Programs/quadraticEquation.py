0c46d1c95f9814491fce8aa9b417029d42839cf3
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2022-03-18 12:55:16 +0530

    ⭐

--- 2. PRACTICE/1. Basic Programs/quadraticEquation.py
+++ 2. PRACTICE/1. Basic Programs/quadraticEquation.py
@@ -1,15 +1,15 @@
 # A program to solve quadratic equation.
 
-from math import sqrt
+from math import sqrt as s
 
 
-a = int(input('Enter the coefficient of a'))
-b = int(input('Enter the coefficient of b'))
-c = int(input('Enter the coefficient of c'))
+a = int(input('Enter the coefficient of a '))
+b = int(input('Enter the coefficient of b '))
+c = int(input('Enter the coefficient of c '))
 
 discriminant = (b**2) - (4 * a * c)
 
-soln1 = (-b + sqrt((b**2) - (4*a*c))) / (2*a)
-soln2 = (-b - sqrt((b**2) - (4*a*c))) / (2*a)
+soln1 = (-b + s((b**2) - (4*a*c))) / (2*a)
+soln2 = (-b - s((b**2) - (4*a*c))) / (2*a)
 
-print(soln1, " ", soln2)
+print("The roots are ", soln1, ",", soln2)
