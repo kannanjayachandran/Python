604d540311093d8c6afb14483df0e0c16ce6f352
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2022-03-25 13:09:52 +0530

    added new program

---
+++ 2. PRACTICE/1. Basic Programs/fibonacci.py
@@ -0,0 +1,17 @@
+# Find the n th Fibonacci number.
+
+num = int(input("Enter the number: "))
+if num == 1 or num == 0:
+    print(num)
+
+else:
+    first, second, count = 0, 1, 2
+    while count <= num:
+        first, second = second, first+second
+        count += 1
+
+print(num, "th fibonacci number is", second)
+
+'''
+    This is not the efficient way for finding fibonacci numbers.
+'''
