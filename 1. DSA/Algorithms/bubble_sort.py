8b1dd2e292106a51954bbf170aa5a9ec677ebac2
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2022-08-01 17:54:44 +0530

    Added bubble sort

--- 1. DSA/Algorithms/bubble_sort.py
+++ 1. DSA/Algorithms/bubble_sort.py
@@ -1,24 +1,27 @@
 # Bubble sort in python
 
-from random import randint as r
+from random import randint
 
-# making a list with 20 random numbers
 lis = []
-for i in range(20):
-    lis.append(r(1, 100))
+for i in range(10):
+    lis.append(randint(1, 100))
 
 print("The list before sorting is: ", lis)
 
-# Bubble sort
+# Bubble sort Algorithm
 for i in range(len(lis)):
     for j in range(len(lis) - i - 1):
         if lis[j] > lis[j + 1]:
             lis[j], lis[j + 1] = lis[j + 1], lis[j]
 
-print('After sorting the list is: ', lis)
+print('\nAfter sorting the list is: ', lis)
 
 """
-Bubble sort also referred to as sinking sort is a simple sorting algorithm that 
-repeatedly steps through the list, compares adjacent elements and swaps them if 
-they are in the wrong order. The pass through the list is repeated until the list is sorted.
+Bubble sort also referred to as sinking sort is a simple sorting 
+algorithm that repeatedly steps through the list, compares adjacent 
+elements and swaps them if they are in the wrong order. The pass 
+through the list is repeated until the list is sorted.
+
+If the array is already sorted bubble sort would take O(n) time.
+O(N^2) is the worst time complexity.
 """
