6c60506688756c854a50e08766778b7755c6ab76
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2022-10-25 18:43:58 +0530

    updated the jupyter notebook creator project

--- 3. PROJECTS/Jupyter Notebook creator/Readme.md
+++ 3. PROJECTS/Jupyter Notebook creator/Readme.md
@@ -1,3 +1,21 @@
 # Jupyter Notebook creator
 
-## This is a simple python script to create Jupyter Notebooks with current date as the name of the notebook. I have compiled the script into an executable file
+**This is a simple python script to create Jupyter Notebooks with current date as the name of the notebook.**
+
+The script would create a folder with the name Jupyter Notebooks and create a Jupyter Notebook with the current date as the name of the notebook.
+
+## How to use
+
+- Download the script.
+- Run the script using python.
+
+## Dependencies
+
+- Python 3.6 or above
+- OS module
+- Datetime module
+
+## TODO
+
+- [ ] Consoliadte the code into a single function and call it from main.
+- [ ] Add a GUI.
--- 3. PROJECTS/Jupyter Notebook creator/main.py
+++ 3. PROJECTS/Jupyter Notebook creator/main.py
@@ -2,10 +2,17 @@ import os
 from datetime import datetime
 
 
+# checking current directory and creating a folder
+current_directory = os.getcwd()
+final_directory = os.path.join(current_directory, r'Jupyter Notebooks')
+if not os.path.exists(final_directory):
+    os.makedirs(final_directory)
+
 today = datetime.today().strftime("%b-%d-%Y")
 
+# Creating Jupyter file with current day as name
 filepath = os.path.join(
-    'C:/Users/kanna/Documents/25. Practice/FILESS', '%s.ipynb' % today)
-if not os.path.exists('C:/Users/kanna/Documents/25. Practice/FILESS'):
-    os.makedirs('C:/Users/kanna/Documents/25. Practice/FILESS')
+    current_directory, final_directory, '%s.ipynb' % today)
+if not os.path.exists(final_directory):
+    os.makedirs(final_directory, today)
 f = open(filepath, "a")
