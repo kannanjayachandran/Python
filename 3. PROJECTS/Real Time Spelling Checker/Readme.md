9db0e19cefb934a45ec7e4aeccf64da5b7495e28
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2022-12-17 22:43:18 +0530

    Added new project

---
+++ 3. PROJECTS/Real Time Spelling Checker/Readme.md
@@ -0,0 +1,5 @@
+# Real Time spelling checker
+
+## This is a real time spelling checker made using python. It uses the NLTK library and a simple regular expression to check the spelling of the words in a sentence
+
+__NeuralNine Youtube Channel__
---
+++ 3. PROJECTS/Real Time Spelling Checker/main.py
@@ -0,0 +1,44 @@
+import re
+import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
+
+import nltk
+from nltk.corpus import words
+
+#nltk.download("words")
+
+class Checker_main:
+
+    def __init__(self):
+
+        self.root = tk.Tk()
+        self.root.geometry("600x500")
+        
+        self.letter = ScrolledText(self.root, font=("Helvetica", 16))
+        self.letter.bind("<KeyRelease>", self.check)
+        self.letter.pack()
+
+        self.previous_spaces = 0
+
+        self.root.mainloop()
+
+
+    def check(self ,event):
+
+        content =self.letter.get("1.0", tk.END)
+        space_count = content.count(" ")
+
+        for part in self.letter.tag_names():
+            self.letter.tag_delete(part)
+
+        if space_count != self.previous_spaces:
+            self.previous_spaces = space_count
+
+            for word in content.split(" "):
+
+                if re.sub(r"[^\w]", '', word.lower()) not in words.words():
+                    position = content.find(word)
+                    self.letter.tag_add(word, f"1.{position}", f"1.{position + len(word)}")
+                    self.letter.tag_config(word, foreground="red")
+
+Checker_main()
