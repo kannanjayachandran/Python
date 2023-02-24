99cf950e2e4c27eb41aa75a4488c56292fda0e9f
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2023-02-24 13:35:14 +0530

    updated to pylint standards

--- 3 Minor projects/Interactive Dictionary/main.py
+++ 3 Minor projects/Interactive Dictionary/main.py
@@ -1,44 +1,50 @@
 import json
 from difflib import get_close_matches
 
-dic_data = json.load(
-    open("3. PROJECTS\Interactive Dictionary\dictionary.json"))
+
+def main():
+    print("\tSimple Dictionary\n")
+    word = input("Enter word: ")
+
+    file_path = "dictionary.json"
+    with open(file_path) as file:
+        dict_data = json.load(file)
+
+    output = translate(word, dict_data)
+
+    if type(output) == list:
+        for i in output:
+            print(f"-> {i}\n")
+    else:
+        print(output)
 
 
-def translate(word):
+def translate(word, dict_data):
+    """
+    This function takes a word as input and returns the meaning of the word.
+    """
 
     word = word.lower()
 
-    if word in dic_data:
-        return dic_data[word]
+    if word in dict_data:
+        return dict_data[word]
 
-    elif len(get_close_matches(word, dic_data.keys())) > 0:
+    elif len(get_close_matches(word, dict_data.keys())) > 0:
         ans = input("Did you mean %s instead ? \nEnter Y if Yes, N if No\t: " %
-                    get_close_matches(word, dic_data.keys())[0])
+                    get_close_matches(word, dict_data.keys())[0])
 
         if ans.lower() == 'y':
-            return dic_data[get_close_matches(word, dic_data.keys())[0]]
+            return dict_data[get_close_matches(word, dict_data.keys())[0]]
 
         elif ans.lower() == 'n':
-            return "Sorry, couldn't find the word, '%s'" % word
+            return f"Sorry, couldn't find the word, '{word}'"
 
         else:
             return "Oops didn't understand that."
 
     else:
-        return "Sorry, couldn't find the word, '%s'" % word
+        return f"Sorry, couldn't find the word, '{word}'"
 
 
-print("\n\t\tSimple Dictionary\n")
-word = input("Enter word: ")
-
-
-output = translate(word)
-
-if type(output) == list:
-    for i in output:
-        print("->", i, "\n")
-else:
-    print(output)
-
-# End of File
+if __name__ == "__main__":
+    main()
