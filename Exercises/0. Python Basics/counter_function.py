79fef9b4b93132f21742b3811c3bde316144bfa8
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2024-03-29 22:47:46 +0530

    Refactor code and add examples for Counter and zip functions

--- Exercises/0. Python Basics/counter_function.py
+++ Exercises/0. Python Basics/counter_function.py
@@ -1,8 +1,34 @@
-# counter in python
-
 from collections import Counter
 
-p = "Python can be easy to pick up whether you're a first time programmer or you're experienced with other languages."
+
+# A counter is a dict subclass for counting iterable objects (Unordered).
+c = Counter()
+print(f"\nA new, empty counter: {c}")
+c = Counter("Hercules")
+print(f"\nA new counter from iterable: {c}")
+c = Counter({"A": 1, "B": 2, "C": 3})
+print(f"\nA new counter from mapping : {c}")
+c = Counter(one=10, two=20)
+print(f"\nA new counter from keywords arguments: {c}")
+
+c = Counter(['one', 'two', 'three'])
+print(f'\n\nNo key error: {c['Four']}')
+c['five'] = 0
+print(f'\nThis won\'t delete element:  {c}')
+del c['five']
+print(f'\nThis will delete: {c}')
+
+c = Counter(a=2, b=3, c=0, d=-10)
+print(f"The counter is : {c}\n\nUsing elements(): ")
+for i in c.elements():
+    print(i, end=" ")
+print(f"\nSorting using sorted(reverse=True): {sorted(c.elements(), reverse=True)}")
+
+c = Counter("Geralt of Rivia")
+print(f"\nMost common : {c.most_common(3)}")
+
+p = """Python can be easy to pick up whether you're a first time programmer or you're
+    experienced with other languages."""
 print(Counter(p.lower()))
 
 q = dict(Counter(p.lower()))
--- Exercises/0. Python Basics/zip.py
+++ Exercises/0. Python Basics/zip.py
@@ -1,7 +1,7 @@
 # zip function takes iterables, and aggregates them into tuples and returns it.
 # We can use Zip function to pack or unpack iterables.
 
-languages = ['Python', 'Java', 'C']
+languages = ["Python", "Java", "C"]
 versions = (1, 2, 3)
 
 output = zip(languages, versions)
@@ -10,3 +10,12 @@ print(output)
 
 # Converting iterator into list.
 print(list(output))
+
+myStr1 = "ABCD"
+myStr2 = "xy"
+myStr3 = zip(myStr1, myStr2)
+print(f"Combined string: {list(myStr3)}")
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+transposed = list(zip(*matrix))
+print(transposed)
